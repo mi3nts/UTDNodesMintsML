a863101211537b94fd7b71c77426c889df1e299b
lakithaomal <you@example.com>
2020-09-23 10:54:09 -0500

    daily Updates

---
+++ firmware/calibration/air930Run.sh
@@ -0,0 +1,31 @@
+LOG_FILE="utdNodes.log"
+echo "[${USER}][`date`] - ${*}" >> ${LOG_FILE}
+matlab -nodesktop -nodisplay utdNodesCalibrator(7)
+
+echo "[${USER}][`date`] - ${*}" >> ${LOG_FILE}
+matlab -nodesktop -nodisplay utdNodesCalibrator(8)
+
+echo "[${USER}][`date`] - ${*}" >> ${LOG_FILE}
+matlab -nodesktop -nodisplay utdNodesCalibrator(9)
+
+echo "[${USER}][`date`] - ${*}" >> ${LOG_FILE}
+matlab -nodesktop -nodisplay utdNodesCalibrator(10)
+
+echo "[${USER}][`date`] - ${*}" >> ${LOG_FILE}
+matlab -nodesktop -nodisplay utdNodesCalibrator(11)
+
+echo "[${USER}][`date`] - ${*}" >> ${LOG_FILE}
+matlab -nodesktop -nodisplay utdNodesCalibrator(12)
+
+echo "[${USER}][`date`] - ${*}" >> ${LOG_FILE}
+matlab -nodesktop -nodisplay utdNodesCalibrator(13)
+
+echo "[${USER}][`date`] - ${*}" >> ${LOG_FILE}
+matlab -nodesktop -nodisplay utdNodesCalibrator(14)
+
+echo "[${USER}][`date`] - ${*}" >> ${LOG_FILE}
+matlab -nodesktop -nodisplay utdNodesCalibrator(15)
+
+
+
+
